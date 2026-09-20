709262982a35926930779e3db950b8c86fc0ed4b
Jose Vilchez <jose.vg_05@hotamil.com>
2026-09-20 13:42:58 -0500

    Conectar lista de tareas con API

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,54 @@
-fetch ('/api/mensaje')
-.then(response => response.json())
-.then(data =>{
-    document.getElementById('mensaje').textContent = data.mensaje;
+const listaTareas = document.getElementById('lista-tareas');
+const formularioTarea= document.getElementById('formulario-tarea');
+const tituloTarea = document.getElementById('titulo-tarea');
 
-})
+async function obtenerTareas() {
+    try {
+        const respuesta = await fetch('/api/tareas');
+        const tareas = await respuesta.json();
 
-.catch(error =>{
-    console.error('Error',error);
-})
+        mostrarTareas(tareas);
+    }catch(error){
+        console.error('Error al obtener tareas:' , error);
+    }
+}
+
+function mostrarTareas(tareas){
+    listaTareas.innerHTML = '';
+
+    tareas.forEach(tarea => {
+        const elemento = document.createElement('li');
+        elemento.textContent = tarea.titulo;
+
+        if(tarea.completada){
+            elemento.style.textDecoration= 'line-through';
+        }
+        listaTareas.appendChild(elemento);
+    });
+}
+
+formularioTarea.addEventListener('submit', async (evento) =>{
+    evento.preventDefault();
+
+    const titulo = tituloTarea.value;
+
+    try{
+        const respuesta = await fetch('/api/tareas',{
+            method: 'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({
+                titulo: titulo
+            })
+        });
+        const nuevaTarea = await respuesta.json();
+        console.log('tarea creada:',nuevaTarea);
+        titulo.value ='';
+        obtenerTareas();
+    }catch(error){
+        console.error('Error al crear la tarea:',error)
+    }
+});
+
+obtenerTareas();
--- server/server.js
+++ server/server.js
@@ -3,12 +3,35 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 app.use(express.static('public'));
 
-app.get('/api/mensaje',(req, res) => {
-    res.json({
-        mensaje: 'hola desde el servidor Node.js'
-    });
+let tareas=[
+    {
+        id: 1,
+        titulo: 'Apredner Node.js',
+        completada: false
+    },
+    {
+        id: 2,
+        titulo: 'Apredner git',
+        completada: true
+    }
+]
+
+app.get('/api/tareas',(req, res) => {
+    res.json(tareas);
+});
+
+app.post('/api/tareas', (req, res) => {
+    const nuevaTarea ={
+        id: tareas.length+1,
+        titulo: req.body.titulo,
+        completada: false
+    };
+    tareas.push(nuevaTarea);
+    res.status(201).json(nuevaTarea);
 });
 
 app.listen(PORT, () => {
